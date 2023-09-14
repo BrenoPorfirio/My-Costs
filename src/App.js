29d2295b1a12b11cdc527c72224a707c8849bbea
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-13 23:32:37 -0300

    update readme

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cost Project
         </a>
       </header>
     </div>
