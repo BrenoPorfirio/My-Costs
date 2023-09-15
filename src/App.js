15e5915bec073db54244c10d2aa1ead6371a2d0f
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-15 00:22:58 -0300

    using uppercase

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 function App() {
 
   const name = "Breno"
+
+  const newName = name.toUpperCase()
   
   return (
     <div className="App">
       <h1>Alterando o JSX</h1>
       <p>Olá, {name}</p>
+      <p>Olá, {newName}</p>
     </div>
   );
 }
