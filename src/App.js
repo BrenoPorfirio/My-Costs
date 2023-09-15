7453b92c6ba86b768acfc45231b5f42357813d52
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-09-15 00:26:13 -0300

    sum function

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ function App() {
   const name = "Breno"
 
   const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b
+  }
   
   return (
     <div className="App">
       <h1>Alterando o JSX</h1>
       <p>Olá, {name}</p>
       <p>Olá, {newName}</p>
-      <p>Soma: {19 + 19}</p>
+      <p>Soma: {sum(12, 54)}</p>
     </div>
   );
 }
